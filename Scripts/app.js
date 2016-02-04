5125c56f53348c9642acc534240be4ac6545f189
ChadClark_Dev <chadclarke@bell.net>
2016-02-04 11:46:41 -0500

    uppdated app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,4 +6,23 @@
   console.log("App Started..."); 
     
     
+    //named function way 
+     function replaceFirstParagraph(){
+         
+        var firstParagraph; 
+        
+        firstParagraph = document.getElementById("firstParagraph");
+        
+        firstParagraph.innerHTML = "MY New Paragraph Data"
+   
+     }
+    // call the replaced first paragrapgh function 
+    replaceFirstParagraph(); 
+    
+    
+
+    
+    
+    
+    
 })();
